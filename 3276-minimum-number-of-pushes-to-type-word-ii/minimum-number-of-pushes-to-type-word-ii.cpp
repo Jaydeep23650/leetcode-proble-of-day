5aa59b2bb5a65a5295689563e907a589c615c4e4
Jaydeep23650 <139055835+Jaydeep23650@users.noreply.github.com>
2024-08-06 14:29:48 +0530

    Time: 72 ms (62.04%) | Memory: 25 MB (57.94%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,28 +1,18 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        map<char,int>mp;
-        for(auto it:word)mp[it]++;
 
-        priority_queue<pair<int,char>>q;
-        for(auto it:mp){
-            q.push({it.second,it.first});
+        vector<int> v(26, 0);
+        for (int it : word) {
+            v[it - 'a']++;
         }
-        int count=0,ans=0;
-        while(!q.empty()){
-            count++;
-            int n=q.top().first;
-            q.pop();
-            if(count<=8){
-                ans+=n;
-            }else if(count>8&&count<17){
-                ans+=n*2;
-            }else if(count>16&&count<25){
-                ans+=3*n;
-            }else{
-                ans+=n*4;
-            }
+        int ans = 0;
+        sort(v.begin(), v.end(), greater<int>());
+        for (int i = 0; i < v.size(); i++) {
+
+            ans += v[i] * (i / 8 + 1);
         }
+
         return ans;
     }
 };
