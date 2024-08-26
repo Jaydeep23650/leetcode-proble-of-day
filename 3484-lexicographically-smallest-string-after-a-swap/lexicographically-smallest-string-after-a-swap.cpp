f5b82a341cccdb4dc73459c32f7f4f04729a9e58
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-08-26 15:05:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (52.18%) - LeetSync

---
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string getSmallestString(string s) {
+        for(int i=0; i<s.size()-1;i++){
+            if((s[i]%2==s[i+1]%2) and s[i]>s[i+1]){
+                swap(s[i],s[i+1]); 
+                break;
+            }
+        }
+        return s;
+    }
+};
