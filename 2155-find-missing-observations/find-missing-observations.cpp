e11850c2c90f97500bde5c09ddc0b507e483e462
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-09-05 19:56:21 +0530

    Time: 106 ms (29.90%) | Memory: 116.4 MB (69.12%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -8,21 +8,16 @@ public:
         int m=rolls.size();
         int total=mean*(m+n);
         sum=total-sum;
-        if(sum<=0||sum>6*n)return {};
+        
         int r=sum/n;
         int t=sum%n;
-        vector<int>ans(n,0);
-        for(int i=0;i<n;i++){
-            if(sum>0){
-                ans[i]=r;
-                sum=sum-r;
-            }
-            if(t>0){
-                ans[i]++;
-                t--;
-            }
+        if(r<=0||sum>n*6)return {};
+        vector<int>ans(n,r);
+        for(int i=0;i<t;i++){
+            ans[i]++;
         }
-        if(ans[n-1]==0)return {};
+        
+        // if(ans[n-1]==0)return {};
         return ans;
     }
 };
