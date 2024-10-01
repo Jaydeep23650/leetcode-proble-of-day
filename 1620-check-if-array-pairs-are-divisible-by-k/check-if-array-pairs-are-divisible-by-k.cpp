314c6a1908c0c690236eb18dba732fce401196ff
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-10-01 17:42:55 +0530

    Time: 91 ms (79.76%) | Memory: 64.4 MB (88.62%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    static bool canArrange(vector<int>& arr, int k) {
-        vector<int> freq(k, 0);
-        for(int x: arr){
-            x%=k;
-            if (x<0) x+=k;
-            freq[x]++;
+    bool canArrange(vector<int>& arr, int k) {
+        vector<int> cnt(k);
+        for (auto x : arr) {
+            cnt[((x % k) + k) % k]++;
         }
-
-        if (k%2==0 && freq[k/2]%2!=0) return 0;
-        int k2=k/2-(k%2==0);
-        for(int i=1; i<=k2; i++)
-            if (freq[i]!=freq[k-i]) return 0;
-        return 1;
+        if (cnt[0] % 2) return false;
+        for (int i = 1; i < k / 2 + (k % 2); i++) { // ceil of k / 2
+            if (cnt[i] != cnt[k-i]) return false;
+        }
+        return true;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
