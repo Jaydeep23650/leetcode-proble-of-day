3a20f663748d33f6f76e1bb265599967f2ddfad4
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-10-19 16:56:29 +0530

    Time: 39 ms (34.30%) | Memory: 53.8 MB (46.42%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
      string reverse1(string &s){
-        string t="";
-        for(int i=s.size()-1;i>=0;i--){
-            if(s[i]=='1')t.push_back('0');
-            else t.push_back('1');
+        int n=s.size();
+        for(int i=0;i<n/2-(n%2!=0);i++){
+            if(s[i]=='1')s[i]='0';
+            else s[i]='1';
+              if(s[n-1-i]=='1')s[n-1-i]='0';
+            else s[n-1-i]='1';
+            swap(s[i],s[n-1-i]);
+        }
+        if(n%2!=0){
+            if(s[n/2]=='0')s[n/2]='1';
+            else s[n/2]='0';
         }
         // reverse(s.begin(),s.end());
-        return t;
+        return s;
      }
     char findKthBit(int n, int k) {
         string curr="0";
