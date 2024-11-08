9acac387a245ce4f6728e908a7619825bf14e8dc
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-11-08 20:19:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (81.57%) - LeetSync

--- 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1441-minimum-flips-to-make-a-or-b-equal-to-c/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
-     vector<int>v1(32,0),v2(32,0),v3(32,0);
-     int i=0;
-     while(a||b||c){
-        v1[i]=(a%2);
-        v2[i]=(b%2);
-        v3[i]=(c%2);
-        i++;
-        a/=2;
-        b/=2;
-        c/=2;
-     }
-     int ans=0;
-      for(int i=0;i<32;i++){
-        if((v1[i]||v2[i])!=v3[i]){
-          if(v1[i]&v2[i])ans=ans+2;
-          else ans++;
+        int ans=0;
+        for(int i=0;i<32;i++){
+            if(c&1){
+                if((a&1)|(b&1)){}
+                else ans++;
+                 a>>=1;
+                b>>=1;
+                c>>=1;
+            }else{
+                if(a&1)ans++;
+                 if(b&1)ans++;
+                a>>=1;
+                b>>=1;
+                c>>=1;
+            }
         }
-      }
-      return ans;
-
+        return ans;
     }
 };
