317c0d9db885ed893cf7434ea93e6f82faa1531f
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-11-10 21:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (44.13%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-     int bitcount(int n){
-        int count=0;
-        while(n){
-            if(1&n)count++;
-            n=n/2;
-        }
-        return count;
-     }
+   
     vector<int> countBits(int n) {
-        vector<int>ans;
-        int i=0;
-        while(i<=n){
-           ans.push_back(bitcount(i++));
+        vector<int>ans(n+1,0);
+        for(int i=1;i<=n;i++){
+            ans[i]=ans[i>>1]+(i&1);
         }
         return ans;
     }
