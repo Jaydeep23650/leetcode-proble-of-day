1c76373d2d4652e53d026b869429b158fc4897b7
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-11-13 00:14:50 +0530

    Time: 75 ms (31.15%) | Memory: 97.2 MB (41.89%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,35 +1,28 @@
-// #include <vector>
-// #include <algorithm>
-
+#include<bits/stdc++.h>
 class Solution {
 public:
-    std::vector<int> maximumBeauty(std::vector<std::vector<int>>& items, std::vector<int>& q) {
-        // Sort items based on price
-        std::sort(items.begin(), items.end());
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& q) {
+        int n=items.size();
+        sort(items.begin(),items.end());
+        vector<int>a(n,0);
+        vector<int>ans(q.size());
+        a[0]=items[0][1];
         
-        int n = items.size();
-        std::vector<int> maxBeauty(n);
-        
-        // Precompute maximum beauty for each price
-        maxBeauty[0] = items[0][1];
-        for (int i = 1; i < n; i++) {
-            maxBeauty[i] = std::max(maxBeauty[i - 1], items[i][1]);
+        for(int i=1;i<n;i++){
+            a[i]=max(items[i][1],a[i-1]);
         }
-        
-        std::vector<int> ans(q.size());
-        
-        for (int i = 0; i < q.size(); i++) {
-            // Use upper_bound to find the rightmost item within the budget
+
+        for(int i=0;i<q.size();i++){
             auto it = std::upper_bound(items.begin(), items.end(), std::vector<int>{q[i], INT_MAX});
-            if (it == items.begin()) {
-                ans[i] = 0; // No items can be bought
-            } else {
-                // Get the index of the last affordable item
-                int idx = std::distance(items.begin(), it) - 1;
-                ans[i] = maxBeauty[idx]; // Maximum beauty for the affordable items
+           
+             if(it==items.begin())ans[i]=0;
+             else{
+            int idx=distance(items.begin(),it)-1;
+            ans[i]=a[idx];
             }
         }
         
+
         return ans;
     }
 };
