3303f3b692abc8b2279c97b6589d2d3f74ced3ba
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-11-16 11:35:20 +0530

    Time: 0 ms (100.00%) | Memory: 33 MB (77.25%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.cpp
@@ -5,9 +5,11 @@ public:
         vector<int>ans(n-k+1,-1);
         for(int i=0;i<=n-k;i++){
               bool track=true;
+            //  check here consecutive increasing value 
             for(int j=i;j<i+k-1;j++){
                 if(nums[j]!=nums[j+1]-1){
                     track=false;
+                    i=j;
                     break;
                 }
               
