9e0a373c2db28324c6b80dbccfdbd7f7eb2547a5
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-11-17 18:33:21 +0530

    Time: 15 ms (84.19%) | Memory: 49.7 MB (85.99%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -5,14 +5,14 @@ public:
         int ans=0,curr=0,i=0;
        while(i<nums.size()){
         int j=0,count=0,curr=nums[i];
+           //count all consecutive element in array;
            for( j=i;j<nums.size();j++){
             if(curr==nums[j]){
                 curr++;
                 count++;
                 ans=max(ans,count);
-            }else if(curr-1==nums[j])continue;
+            }else if(curr-1==nums[j])continue;//skip all equal elements
             else{
-                //  i=j-1;
                 break;
             }
            }
