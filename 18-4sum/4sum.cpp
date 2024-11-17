119ba3fe1ae3db2ce28fe967dc4f861cadb25339
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-11-17 17:53:30 +0530

    Time: 23 ms (59.28%) | Memory: 16.1 MB (71.03%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,27 +1,46 @@
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>>ans;
-        sort(begin(nums),end(nums));
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(i>0&&nums[i]==nums[i-1])continue;
-            for(int j=i+1;j<n;j++){
-                if(j>i+1&&nums[j]==nums[j-1])continue;
-                int left=j+1,right=n-1;
-                while(right>left){
-                  long long sum=(long long)nums[i]+nums[j]+nums[left]+nums[right];
-                  if(sum==target){
-                    ans.push_back({nums[i],nums[j],nums[left],nums[right]});
-                    while(left<right&&nums[left]==nums[left+1])left++;
-                     while(left<right&&nums[right]==nums[right-1]){right--;}
-                     left++;right--;
-                  }else if(sum>target)right--;
-                  else left++;
-                }
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
 
+        for (int i = 0; i < n; i++) {
+            // Skip duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n; j++) {
+                // Skip duplicates for the second number
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1;
+                int right = n - 1;
+
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        // Skip duplicates for the third number
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        // Skip duplicates for the fourth number
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
             }
         }
+
         return ans;
     }
 };
