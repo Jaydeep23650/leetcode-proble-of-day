f1c705c6b8c97a5137f3d658c69232734be4f35d
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2024-11-22 12:24:15 +0530

    Time: 20 ms (77.54%) | Memory: 83.3 MB (19.40%) - LeetSync

--- 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.cpp
@@ -5,6 +5,7 @@ public:
         for(auto it:matrix){
             string s="";
             int temp=it[0];
+            //compare with first value of vector in intire vector where value are equa and where are not
             for(auto i:it){
                 if(i==temp)s.push_back('s');
                 else s.push_back('B');
@@ -12,6 +13,7 @@ public:
             mp[s]++;
         }
         int ans=0;
+        //store max ans
         for(auto it:mp){
             ans=max(ans,it.second);
         }
