29bafb3e5ad581530b2e39e61f9ef812616f826e
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2025-02-26 22:17:28 +0530

    Time: 3 ms (20.73%) | Memory: 71.8 MB (18.20%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,15 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        long long ans=nums[0],sum=0;
-        for(auto it:nums){
-            sum+=it;
-             ans=max(ans,sum);
-            if(sum<0){
-                sum=0;
+        int ans = nums[0]; // Initialize ans with the first element
+        int sum = 0; // This will hold the current subarray sum
+        
+        for (auto it : nums) {
+            sum += it; // Add the current element to the current sum
+            ans = max(ans, sum); // Update the maximum found so far
+            
+            // If the current sum becomes negative, reset it to 0
+            if (sum < 0) {
+                sum = 0;
             }
-           
         }
-        return ans;
+        
+        return ans; // Return the maximum subarray sum
     }
 };
