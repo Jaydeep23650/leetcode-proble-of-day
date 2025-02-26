59397cfc45ba453544df2eedd2c0f3c56b8623ed
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2025-02-26 14:46:59 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (51.09%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -4,16 +4,20 @@ public:
     int maxAbsoluteSum(vector<int>& nums) {
         int ans=INT_MIN;
         int sum=0;
+        // first colculate highest sum using kadane
         for(auto it:nums){
                if(sum<0)sum=0;
             sum+=it;
          
             ans=max(ans,sum);
         }
+        // for absolute -1 multiply each elements 
         for(int i=0;i<nums.size();i++){
             nums[i]=-nums[i];
         }
+
         sum=0;
+        // first colculate highest sum using kadane .. this done for find lowest sum 
         for(auto it:nums){
              if(sum<0)sum=0;
             sum+=it;
