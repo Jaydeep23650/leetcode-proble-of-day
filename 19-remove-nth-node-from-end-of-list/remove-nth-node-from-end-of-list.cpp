c2f396a3efd3067e0aa6399baf974726dce08c71
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2025-03-23 15:07:25 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (59.72%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,26 +10,47 @@
  */
 class Solution {
 public:
+      
+    //   ListNode* solve1(ListNode* head, int n){
+    //     ListNode* temp=head;
+    //     ListNode* dummy = new ListNode(-2);
+    //     dummy->next=head;
+    //     ListNode* t=dummy;
+    //     if(head==NULL)return NULL;
+    //     while(n--){
+    //         temp=temp->next;
+    //     }
+    //     while(temp){
+    //         temp=temp->next;
+    //         dummy=dummy->next;
+    //     }
+    //         dummy->next=dummy->next->next;
+    //     return t->next;
+        
+    //   }
+
+   
+      ListNode* solve2(ListNode* head, int n){
+        if(head==NULL)return head;
+        ListNode *temp1=head,*temp2=head;
+        ListNode* dummy =new ListNode(-1);
+        dummy->next=head;
+        temp2=dummy;
+
+          while(n--){
+            temp1=temp1->next;
+          }
+          while(temp1){
+            temp1=temp1->next;
+            temp2=temp2->next;
+          }
+         temp2->next=temp2->next->next;
+          return dummy->next;
+      }
+
     ListNode* removeNthFromEnd(ListNode* head, int n) {
       
-        ListNode* temp=head;
-       ListNode* dummy = new ListNode(-2);
-         dummy->next=head;
-         ListNode* t=dummy;
-        if(head==NULL)return NULL;
-        while(n--){
-            temp=temp->next;
-        }
-        while(temp){
-            temp=temp->next;
-            dummy=dummy->next;
-        }
-        // if(dummy->next){
-            // dummy->val=dummy->next->val;
-
-            dummy->next=dummy->next->next;
-        // }
-        return t->next;
-        
+      return solve2(head,n);
+      
     }
 };
