711b03359c2b56eff3be665a752278e83b4b244b
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2025-03-23 13:34:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (69.80%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -10,7 +10,7 @@
  */
 class Solution {
 public:
-   
+    // using recursive methode
    ListNode* solve(ListNode * node){
     if(node==NULL||node->next==NULL)return node;
     ListNode* temp=solve(node->next);
@@ -18,8 +18,23 @@ public:
     node->next=NULL;
     return temp;
    }
+    // solve by using dummy node 
+   ListNode* solve2(ListNode *node){
+    if(node==NULL)return node;
+    ListNode* temp,*temp2,*dummy(0);
+    temp=node;
+    temp2=NULL;
+    while(temp){
+        ListNode* l=temp->next;
+        temp->next=temp2;
+        temp2=temp;
+        temp=l;
+        
+    }
+    return temp2;
+   }
 
     ListNode* reverseList(ListNode* node) {
-        return solve(node);
+        return solve2(node);
     }
 };
