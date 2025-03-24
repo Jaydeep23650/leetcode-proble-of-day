f828d2f650ad627263302ef0a28b5384a1d2f951
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2025-03-24 10:49:34 +0530

    Time: 71 ms (53.06%) | Memory: 126.5 MB (80.10%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.cpp
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -4,15 +4,19 @@ public:
         sort(meetings.begin(),meetings.end());
         int start=meetings[0][0],end=meetings[0][1];
         int ans=0,n=meetings.size();
+        // check how many days left in begining
         if(start>1&&days>start)ans+=start-1;
         for(int i=1;i<n;i++){
+
             if(days<=end)break;
+            // check skip day
             if(meetings[i][0]>end){
                 ans+=meetings[i][0]-(1+end);
             }
             
             end=max(end,meetings[i][1]);
         }
+         // 
         if(days>end)ans+=days-end;
        return ans;
 
