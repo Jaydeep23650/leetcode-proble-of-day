9864cab2a9e612cdd4dbf47417d36b623d61891d
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2025-03-27 15:22:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (44.85%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,31 +1,21 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
+   vector<vector<int>>ans;
+   void solve(vector<int>& nums,vector<int>& temp,int start){
+      if(start>nums.size())return ;
+        ans.push_back(temp);
+      for(int i=start;i<nums.size();i++){
+        if(i>start&&nums[i]==nums[i-1])continue;
+        temp.push_back(nums[i]);
+        solve(nums,temp,i+1);
+        temp.pop_back();
+      }
+    
+   }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> subset;
-        sort(nums.begin(), nums.end()); // Sort to handle duplicates
-        backtrack(result, subset, nums, 0);
-        return result;
-    }
-
-private:
-    void backtrack(vector<vector<int>>& result, vector<int>& subset, 
-                   vector<int>& nums, int start) {
-        result.push_back(subset); // Add the current subset to the result
-
-        for (int i = start; i < nums.size(); i++) {
-            // Skip duplicates
-            if (i > start && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            subset.push_back(nums[i]); // Include the current number
-            backtrack(result, subset, nums, i + 1); // Move to the next index
-            subset.pop_back(); // Backtrack
-        }
+        vector<int>temp;
+        sort(nums.begin(),nums.end());
+        solve(nums,temp,0);
+        return ans;
     }
 };
