79c0c9fb484c085d58721cbb64c33a91aac414a4
Jaydeep <139055835+Jaydeep23650@users.noreply.github.com>
2025-04-01 16:46:51 +0530

    Time: 7 ms (52.63%) | Memory: 21.2 MB (13.96%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,47 +1,36 @@
 class Solution {
 public:
-    bool dfs(int u, vector<int>& vis, vector<int>& recStack, map<int, list<int>>& adj) {
-        // Mark the current node as visited and add to recursion stack
-        vis[u] = 1;
-        recStack[u] = 1;
-
-        // Recur for all the vertices adjacent to this vertex
-        for (auto it : adj[u]) {
-            // If the node is not visited, recurse on it
-            if (!vis[it]) {
-                if (dfs(it, vis, recStack, adj)) {
-                    return true; // Cycle detected
-                }
-            }
-            // If the node is in the recursion stack, a cycle is found
-            else if (recStack[it]) {
-                return true; // Cycle detected
-            }
-        }
-
-        // Remove the vertex from recursion stack
-        recStack[u] = 0;
-        return false; // No cycle detected
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses, 0); // Visited array
-        vector<int> recStack(numCourses, 0); // Recursion stack
-        map<int, list<int>> adj; // Adjacency list
-
-        // Build the adjacency list
-        for (auto it : prerequisites) {
-            adj[it[1]].push_back(it[0]);
+        map<int,vector<int>>adj;
+        int n=numCourses;
+        vector<int>n_dr(n,0);
+        for(auto it:prerequisites){
+            int a=it[0];
+            int b=it[1];
+            adj[b].push_back(a);
+            n_dr[a]++;
         }
-
-        // Check for cycles in different components of the graph
-        for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (dfs(i, vis, recStack, adj)) {
-                    return false; // Cycle detected
+        int count=0;
+        queue<int>q;
+        for(int i=0;i<n_dr.size();i++){
+            if(n_dr[i]==0){
+                count++;
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            for(auto it:adj[node]){
+                n_dr[it]--;
+                if(n_dr[it]==0){
+                    q.push(it);
+                    count++;
                 }
             }
         }
-        return true; // No cycles detected, can finish all courses
+       if(count!=numCourses)return false;
+       return true;
+
     }
 };
